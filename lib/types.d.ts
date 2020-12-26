6b81ebf6920783a458e61a64069184ec1e49d167
Robin <hello@robinblomberg.se>
2020-12-26 11:14:10 +0100

    Add compiler

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,3 +1,7 @@
+import { AST } from '@robinblomberg/sqlite-ast';
+
+declare function compile(node: AST.Node): string;
+
 export type Token = {
   type: 'Hex' | 'Identifier' | 'Keyword' | 'Numeric' | 'Punctuator' | 'String';
   value: string;
